0916bd1e8568e4845e8e5ac5e83b39437498fe7e
Andre Buettemeyer <Andre.Buettemeyer@gmx.de>
2023-11-12 16:34:09 +0100

    Create install_putty.ps1
    Install putty on the guest VM

---
+++ install_putty.ps1
@@ -0,0 +1,22 @@
+#Install Active-Directory
+#Variable $vm_guestname with the virtuellen maschine name must be defined.
+#Variable $windows_adminname must be defined.
+#Variable $windows_password with the administrator password must be defined.
+#Variable $windows_user must be defined.
+#Variable $putty_file with the Path do the installationfile
+
+write-host 'Install putty'
+$vmliste = VBOXManage list vms --long
+$vmvorhanden = $vmliste.contains('Name:                        ' + $vm_guestname)
+if ( $vmvorhanden -eq 'True')
+	{
+		Start-Sleep -Seconds 1
+		VBoxManage guestcontrol $vm_guestname copyto --username $windows_adminname --password $windows_password --target-directory C:\Users\$windows_user'\Downloads\putty.msi' --recursive --verbose  $putty_file
+		Start-Sleep -Seconds 1
+		VBoxManage guestcontrol  $vm_guestname run --username $windows_adminname --password $windows_password --wait-stdout --wait-stderr --exe C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\Users\$windows_user'\Downloads\putty.msi /qn /norestart'
+		./create_link.ps1 -source "'C:\Program Files\PuTTY\putty.exe'" -link "'C:\Users\$windows_user\Desktop\PuTTY.lnk'"		
+	}
+else
+	{
+		write-host $vm_guestname' does not exists. First start installation.'
+	}
