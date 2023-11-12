1c9067aac2d370589e6d6f00170a53286d0ca87d
Andre Buettemeyer <Andre.Buettemeyer@gmx.de>
2023-11-12 16:35:42 +0100

    Update windows_reboot.ps1
    Refactor messsages

--- windows_reboot.ps1
+++ windows_reboot.ps1
@@ -1,7 +1,7 @@
 #Reboot Windows VM
+#Virtualbox Guest Additions must be installed.
+#Variable $vm_guestname with the virtuellen maschine name must be defined.
 
-#Shutdown
+write-host 'Reboot the VM'$vm_guestname
 ./windows_shutdown.ps1
-
-#Start
 ./windows_start.ps1
