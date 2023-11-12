ff469210880ba402307483a8c849ee2295f4bcd5
Andre Buettemeyer <Andre.Buettemeyer@gmx.de>
2023-11-12 16:35:23 +0100

    Update vm_shutdown.ps1
    Refactor messages

--- vm_shutdown.ps1
+++ vm_shutdown.ps1
@@ -2,13 +2,13 @@
 #Requirements:
 #Variable $vm_guestname with the virtuellen maschine name must be defined.
 
-#Shutdown
+write-host 'Power off the VM'$vm_guestname
 vboxmanage controlvm $vm_guestname poweroff
 $shutdowndone = 'False'
 while ( $shutdowndone -ne 'True')
 	{
 		$vminfo = vboxmanage showvminfo $vm_guestname --machinereadable
 		$shutdowndone = $vminfo.contains('VMState="poweroff"')
-		write-host 'Shutdown not yet done. Wait 10 seconds ...'
-		Start-Sleep -Seconds 10
+		write-host 'Shutdown not yet done. Wait 1 minute ...'
+		Start-Sleep -Seconds 60
 	}
